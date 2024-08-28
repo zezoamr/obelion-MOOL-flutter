d0fea53f8e6940a24c2ff7c4393763f98b0bdb03
ziad amr shalaby <zezoamr@yandex.com>
2024-08-28 16:59:59 +0300

    Create confirmcode_screen.dart

---
+++ flutter_mool/lib/screens/confirmcode_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ConfirmCodeScreen extends StatelessWidget {
+  const ConfirmCodeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 46, 46, 51),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context); // Handle back button press
+          },
+          child: Image.asset(
+            'assets/images/account/arrowback.png',
+            width: 24,
+            height: 24,
+          ),
+        ),
+        title: const Text(
+          'Verification',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 100),
+            const Expanded(
+              flex: 8,
+              child: ConfirmCodeForm(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ConfirmCodeForm extends StatefulWidget {
+  const ConfirmCodeForm({Key? key}) : super(key: key);
+
+  @override
+  _ConfirmCodeFormState createState() => _ConfirmCodeFormState();
+}
+
+class _ConfirmCodeFormState extends State<ConfirmCodeForm> {
+  final List<FocusNode> _focusNodes = List.generate(4, (index) => FocusNode());
+  final List<TextEditingController> _controllers =
+      List.generate(4, (index) => TextEditingController());
+
+  @override
+  void dispose() {
+    for (var node in _focusNodes) {
+      node.dispose();
+    }
+    for (var controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 237, 237, 237),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const SizedBox(height: 20),
+          Image.asset(
+            'assets/images/account/verf.png',
+            width: 200,
+            height: 200,
+          ),
+          const Text(
+            'Enter 4 Digits code sent to',
+            style: TextStyle(fontSize: 16, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          const Text(
+            '+961518745363',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          //_buildCodeInputField(),
+          const SizedBox(height: 20),
+          const Text(
+            'Resend code in 00:59',
+            style: TextStyle(fontSize: 14, color: Colors.grey),
+            textAlign: TextAlign.center,
+          ),
+          const Spacer(flex: 2),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black,
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onPressed: () {},
+            child: const Text('Submit'),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
