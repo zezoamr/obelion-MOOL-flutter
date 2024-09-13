9359254979c04c623c7b522e641efee533d8f452
ziad amr shalaby <zezoamr@yandex.com>
2024-09-13 08:48:50 +0300

    Create user_data.dart

---
+++ flutter_mool/lib/settings/data/user_data.dart
@@ -0,0 +1,15 @@
+class UserData {
+  final String name;
+  final String email;
+  final String phone;
+  final String dateOfBirth;
+  final String password;
+
+  UserData({
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.dateOfBirth,
+    required this.password,
+  });
+}
